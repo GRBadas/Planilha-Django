51a66ade3fc92845715f69d3086dba2533dba70f
Badas <grbadas@gmail.com>
2025-03-28 01:21:07 -0300

    style

--- frontend/planilha/src/App.tsx
+++ frontend/planilha/src/App.tsx
@@ -3,17 +3,23 @@ import CartaoList from './components/CartaoList';
 import CategoriaList from './components/CategoriaList';
 import TransacaoList from './components/TransacaoList';
 import NovaTransacao from './components/NovaTransação';
+import Sidebar from './components/Sidebar';
 
 const App: React.FC = () => {
   return (
-    <div className='flex bg-zinc-800 flex-col items-center'>
-      <h1 className='text-3xl font-semibold text-center text-gray-300'>Gestão de Gastos</h1>
-      <div className='mt-30 flex'>
-        <NovaTransacao />
-        <CartaoList />
-        <CategoriaList />
-        <TransacaoList />
+    <div>
+      <Sidebar />
+    <div className="flex">
+      <div className="flex-1 bg-zinc-800 flex flex-col items-center p-4">
+        <h1 className="text-3xl font-semibold text-center dark:text-violet-600">Gestão de Gastos</h1>
+        <div className="mt-6 flex flex-wrap justify-center gap-4">
+          <NovaTransacao />
+          <CartaoList />
+          <CategoriaList />
+          <TransacaoList />
+        </div>
       </div>
+    </div>
     </div>
   );
 };
--- frontend/planilha/src/components/NovaTransação.tsx
+++ frontend/planilha/src/components/NovaTransação.tsx
@@ -162,7 +162,7 @@ const NovaTransacao: React.FC = () => {
 
         <button
           type="submit"
-          className="w-full py-2 px-4 bg-indigo-600 text-white font-semibold rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          className="w-full py-2 px-4 bg-violet-600 text-white font-semibold rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
         >
           Adicionar Transação
         </button>
